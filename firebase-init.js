83a73fcb8b707c44608ca7cc4d487ced97f6ca6e
Thiago130589 <thiagoferreira2flores@gmail.com>
2025-10-07 00:21:00 -0300

    FINAL: Corrigido carregamento modular de HTML/JS e verificada chave API

--- firebase-init.js
+++ firebase-init.js
@@ -1,7 +1,6 @@
 // firebase-init.js
 
-// Importa as funções específicas. O navegador SABE onde buscar
-// porque as URLs do CDN estão no HTML!
+// Importa as funções específicas. O navegador as encontra porque o CDN está no HTML.
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics"; 
 import { getAuth } from "firebase/auth"; 
@@ -9,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Sua configuração do aplicativo Web do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyD2ZVd8TyXZN792UjTqaFGw9OPcEp7JPzw",
-  authDomain: "projetofamilia-f0190.firebaseapp.com",
-  projectId: "projetofamilia-f0190",
-  storageBucket: "projetofamilia-f0190.firebasestorage.app",
-  messagingSenderId: "725318208083",
-  appId: "1:725318208083:web:e5d77fb6ebea7a64b914a6",
-  measurementId: "G-M6TH4JTMM5"
+    apiKey: "AIzaSyD2ZVd8TyXZN792UjTqaFGw9OPcEp7JPzw", // Chave correta
+    authDomain: "projetofamilia-f0190.firebaseapp.com",
+    projectId: "projetofamilia-f0190",
+    storageBucket: "projetofamilia-f0190.firebasestorage.app",
+    messagingSenderId: "725318208083",
+    appId: "1:725318208083:web:e5d77fb6ebea7a64b914a6",
+    measurementId: "G-M6TH4JTMM5"
 };
 
 // 1. Inicializa o Firebase App
